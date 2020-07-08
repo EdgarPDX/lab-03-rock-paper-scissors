c9173035b3d67f07d588e51f0a6e4e380e50c377
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-07-08 15:45:44 -0700

    added DOMS and global state

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-// import functions and grab DOM elements
+// import functions 
+
+//DOM elements
+const button = document.getElementById('button');
+const showWins = document.getElementById('wins');
+const showLosses = document.getElementById('losses');
+const showDraws = document.getElementById('draws');
+const didYouWin = document.getElementById('outcome');
 
 // initialize state
+let wins = 0;
+let draws = 0;
+
 
 // set event listeners to update state and DOM
